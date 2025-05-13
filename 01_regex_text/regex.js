ce189061368890c9de5702731be1eb76586c3e80
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2025-05-12 21:09:02 -0300

    treinando

--- 01_regex_text/regex.js
+++ 01_regex_text/regex.js
@@ -21,3 +21,18 @@ console.log(reg2.test(futebol));
 
 console.log(/jesus/.test("jesus cristo de nasare"));
 console.log(/jesus/.test("nossa senhora de aparecida"));
+
+// testando 
+
+let palavra = new RegExp("função");
+console.log(palavra.test("existe a função"));
+
+//maneira dois
+
+let pl = /função/
+console.log(pl.test("cade vc fazendo a função que madei"))
+
+//maneira tres
+
+let lista = "função"
+console.log(pl.test(lista));
